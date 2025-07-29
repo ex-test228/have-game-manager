47ab654048485dfa1953452b49452b509387019b
ex-test228 <magt9014@gmail.com>
2025-07-29 16:46:27 +0900

    second commit

---
+++ app/src/main/java/com/example/app/Application.java
@@ -0,0 +1,55 @@
+package com.example.app;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+// import com.example.app.FormData;
+
+
+@Controller
+public class Application {
+
+    @GetMapping("/")
+    public String main(@RequestParam(name = "name", required = false, defaultValue = "Guest")
+      String game, org.springframework.ui.Model model){
+ 
+    String[] games = {"game 1", "game 2", "game 3", "game 4"};
+    model.addAttribute("games", games);
+
+        return "main"; 
+    }
+
+    // @PostMapping("/add")
+    // public String submitForm(@ModelAttribute("formData") FormData formData, org.springframework.ui.Model model) {
+
+    //     // フォームデータを処理するロジックをここに追加
+    //     String gameName = formData.getName();
+    //     // 例えば、ゲーム名をリストに追加するなどの処理
+
+    //     // モデルにゲーム名を追加
+    //     model.addAttribute("gameName", gameName);
+
+    //     System.out.println(gameName);
+
+    //     // メインページにリダイレクト
+    //     return "redirect:/";
+    // }
+
+    @PostMapping("/add")
+    @ResponseBody // HTMLを返さずに文字列を直接ブラウザに表示
+    public String handleSubmitForm(
+            @ModelAttribute FormData gameName) {
+
+        // 受け取ったデータを処理
+        System.out.println(gameName);
+
+
+        return "ゲーム名 '" + gameName + "' が追加されました。";
+
+
+  }
+}
+
+
---
+++ app/src/main/java/com/example/app/FormData.java
@@ -0,0 +1,21 @@
+package com.example.app;
+
+public class FormData {
+    private String gameName;
+    private String number;
+
+    public String getName() {
+        return gameName;
+    }
+
+    public void setName(String gameName) {
+        this.gameName = gameName;
+    }
+        public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
